cc283556cf7b5fbfc03ac1a6ccfa1017314217cf
jesse10930 <ronda.jesse@gmail.com>
2021-08-31 10:19:58 -0400

    fixing build error caused by leaflet

--- src/components/map.js
+++ src/components/map.js
@@ -1,18 +1,21 @@
 import React from "react"
 import { MapContainer, TileLayer, Marker } from "react-leaflet"
 
-const map = ({ center }) => {
-  return (
-    <div>
-      <MapContainer center={center} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={center}></Marker>
-      </MapContainer>
-    </div>
-  )
+const Map = ({ center }) => {
+  if (typeof window !== "undefined") {
+    return (
+      <div>
+        <MapContainer center={center} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={center}></Marker>
+        </MapContainer>
+      </div>
+    )
+  }
+  return null
 }
 
-export default map
+export default Map
